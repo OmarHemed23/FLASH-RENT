9967dbba6cf6361e3a742bbce97b085c20d293c8
Omar Hemed <omarhemed800@gmail.com>
2024-06-08 18:27:08 +0300

    feat: Implement GuestLayout component for guest user interface - The component utilizes the following components:     - `Navbar`: Renders the navigation bar with links relevant to guest users (Home, About, Login).     - `Sidebar`: Provides a responsive sidebar menu for smaller screens (displays the same links as Navbar for guests).     - `useSidebar` hook: Manages the visibility state of the sidebar.     - `CustomNavLink`: Used for navigation links within the Navbar (large screens view).     - `ResponsiveNavLink`: Used for navigation links within the Sidebar (mobile view). - Renders the content of the guest user interface using the provided `children` prop.

---
+++ Frontend/flash-rent/src/layout/GuestLayout.jsx
@@ -0,0 +1,39 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { useSidebar } from "../hooks/useSideBar";
+import CustomNavLink from "../components/NavLink";
+import ResponsiveNavLink from "../components/ResponsiveNavLink";
+
+export default function GuestLayout ({children}) {
+    const { showingSidebar, openSidebar, closeSidebar } = useSidebar();
+
+    const NavbarLinks = [
+        { path: "/", name: "Home" },
+        { path: "about", name: "About" },
+        { path: "/auth/login", name: "Login"}
+    ];
+
+    return (
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <Navbar openSidebar={openSidebar} showingSidebar={showingSidebar}>
+                <div className="hidden md:flex md:items-center">
+                    {NavbarLinks.map((link, index) => (
+                        <CustomNavLink key={index} to={link.path} className="inline-flex items-center px-1 pt-1 dark:text-gray-100">
+                            {link.name}
+                        </CustomNavLink>
+                    ))}
+                </div>
+            </Navbar>
+            <Sidebar showingSidebar={showingSidebar} className="md:hidden">
+                {NavbarLinks.map((link, index) => (
+                    <ResponsiveNavLink key={index} to={link.path} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        {link.name}
+                    </ResponsiveNavLink>
+                ))}
+            </Sidebar>
+            <main className="flex flex-col pt-6 p-6 md:p-10 space-y-6 mt-10">
+                {children}
+            </main>
+        </div>
+    );
+};
