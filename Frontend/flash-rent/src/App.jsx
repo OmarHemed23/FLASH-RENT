b3758ee2e171a5cde8874e3823bec417ce52c32a
Omar Hemed <omarhemed800@gmail.com>
2024-06-09 13:33:41 +0300

    feat: Integrate React Router for Navigation

--- Frontend/flash-rent/src/App.jsx
+++ Frontend/flash-rent/src/App.jsx
@@ -1,8 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
+import Router from './routes/Routes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <RouterProvider router={Router}/>
     </div>
   );
 }
