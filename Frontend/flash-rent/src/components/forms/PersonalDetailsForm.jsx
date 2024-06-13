c3a0135bb01d96aae6be17f12c55c80c0973b90e
Omar Hemed <omarhemed800@gmail.com>
2024-06-13 16:39:40 +0300

    feat: Add PersonalDetailsForm for collecting user information -Uses FormField components for structured input fields. -Binds form fields to state (propertyName) and update function (updateFormData). -Handles user input changes and updates application data.

---
+++ Frontend/flash-rent/src/components/forms/PersonalDetailsForm.jsx
@@ -0,0 +1,26 @@
+import FormField from '../FormField';
+
+export default function PersonalDetailsForm({ propertyName, updateFormData }) {
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        updateFormData({
+            [name]: value
+        });
+    };
+
+    return (
+        <div className="grid gap-4 md:grid-cols-2 md:gap-6">
+            <div className="md:col-span-2">
+                <FormField label="Property Applying For" type="text" name="property-name" id="property-name" placeholder="Property Applying" value={propertyName} readOnly />
+            </div>
+            <FormField label="First Name" type="text" name="first-name" id="first-name" placeholder="Your First Name" required onChange={handleInputChange} />
+            <FormField label="Last Name" type="text" name="last-name" id="last-name" placeholder="Your Last Name" required onChange={handleInputChange} />
+            <FormField label="Email" type="email" name="email" id="email" placeholder="Your Email" required onChange={handleInputChange} />
+            <FormField label="Phone Number" type="text" name="phone-number" id="phone-number" placeholder="Your Phone Number" required onChange={handleInputChange} />
+            <FormField label="Address" type="text" name="address" id="address" placeholder="Your Address" required onChange={handleInputChange} />
+            <FormField label="City" type="text" name="city" id="city" placeholder="Your City" required onChange={handleInputChange} />
+        </div>
+    );
+};
+
+
