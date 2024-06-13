861bc833fa146e7bc8ddb6a786442756659f6a04
Omar Hemed <omarhemed800@gmail.com>
2024-06-13 16:35:42 +0300

    feat: Implement FormField component for versatile form inputs - Accepts a `label` prop to display a descriptive label for the field. - Supports various input types through the `type` prop (defaults to "text"). - Binds to a `name` prop for associating the field with form data. - Includes an `id` prop for the input element and label association. - Provides a default `placeholder` for guiding the user on expected input. - Handles required fields with the `required` prop (defaults to false). - Offers read-only mode through the `readOnly` prop (defaults to false). - Supports multiline text input with the `rows` prop (specific to textarea type). - Takes an `options` array for defining options in select input type. - Spreads remaining props for flexibility in customizing the input element.

---
+++ Frontend/flash-rent/src/components/FormField.jsx
@@ -0,0 +1,38 @@
+import Label from "./Label";
+import TextInput from "./TextInput";
+import TextArea from "./TextArea";
+import SelectInput from "./SelectInput";
+
+export default function FormField ({ label, type = "text", name, id, placeholder, required = false, readOnly = false, rows, 
+options = [], ...props }) {
+    return (
+        <div className="w-full">
+            <Label htmlFor={id} className="mb-2">
+                {label}
+            </Label>
+            {type === 'textarea' ? (
+                <TextArea {...props} id={id} rows={rows} placeholder={placeholder} />
+            ) : type === 'select' ? (
+                <SelectInput
+                    {...props}
+                    id={id}
+                    name={name}
+                    options={options}
+                    placeholder={placeholder}
+                    required={required}
+                />
+            ) : (
+                <TextInput
+                    {...props}
+                    type={type}
+                    name={name}
+                    id={id}
+                    className="block w-full p-2.5"
+                    placeholder={placeholder}
+                    required={required}
+                    readOnly={readOnly}
+                />
+            )}
+        </div>
+    );
+};
