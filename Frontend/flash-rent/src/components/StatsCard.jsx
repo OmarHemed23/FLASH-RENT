488b0568c8293134ed67a66b0a994a793469255c
Omar Hemed <omarhemed800@gmail.com>
2024-06-17 19:34:17 +0300

    feat: Implement StatsCard component for data visualization
    -Introduces the StatsCard component for displaying data statistics with:
        -Customizable background color (default white)
        -SVG icon integration (icon prop)
        -Title (title prop)
        -Content area for additional data (children prop)

---
+++ Frontend/flash-rent/src/components/StatsCard.jsx
@@ -0,0 +1,26 @@
+export default function StatsCard ({icon, title, children, bgColor = "white"}) {
+    let bgColorClassName = "";
+
+    if (bgColor === "white") {
+        bgColorClassName = "bg-white";
+    } else if (bgColor === "green") {
+        bgColorClassName = "bg-green-300";
+    } else if (bgColor === "blue") {
+        bgColorClassName = "bg-blue-300";
+    } else if (bgColor === "gray") {
+        bgColorClassName = "bg-gray-300"
+    }
+    return (
+        <div className={`p-8 h-30 md:h-64 flex items-center justify-center shadow-md rounded-lg ${bgColorClassName}`}>
+            <div ClassName="inline-flex flex-shrink-0 items-center justify-center rounded-lg mr-6">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="h-10 w-10 text-gray-900" viewBox="0 0 576 512">
+                    <path d={icon}/>
+                </svg>
+            </div>
+            <div className="ml-5">
+            <h2 className="block text-lg font-bold">{title}</h2>
+            {children}
+            </div>
+        </div>
+    );
+};
