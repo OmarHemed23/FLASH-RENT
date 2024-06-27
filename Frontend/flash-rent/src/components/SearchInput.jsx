97525315877161bc6a740eb378439bc7437a05c1
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-06-27 16:33:37 +0300

    refactor: Enhance SearchInput component with spread operator
    - Added spread operator to pass down additional props to the input element.

--- Frontend/flash-rent/src/components/SearchInput.jsx
+++ Frontend/flash-rent/src/components/SearchInput.jsx
@@ -1,4 +1,4 @@
-export default function SearchInput ({className='',  onSubmit }) {
+export default function SearchInput ({className='',  onSubmit, ...props }) {
     return (
         <form onSubmit={onSubmit} className={"flex items-center" + className}>
             <div className="relative w-full">
@@ -7,7 +7,7 @@ export default function SearchInput ({className='',  onSubmit }) {
                         <path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
                     </svg>
                 </div>
-                <input type="text" name="search-input" id="search-input" placeholder="Search for properties" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 
+                <input {...props} type="text" name="search-input" id="search-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 
                 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
                 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" />
             </div>
