a7fe4c92adb315aed4c88415182514389f44181f
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-06-27 16:52:01 +0300

    feat: Update sidebar navigation links
    - Added new navigation link entries for '/tenant/tenant-talk' and '/tenant/document'.

--- Frontend/flash-rent/src/layout/AuthenticatedLayout.jsx
+++ Frontend/flash-rent/src/layout/AuthenticatedLayout.jsx
@@ -30,7 +30,7 @@ const navLinks = [
         </svg>
         )
     },
-    {   to: "/messages", label: "Messages", icon: (
+    {   to: "/tenant/tenant-talk", label: "Tenant Talk", icon: (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
             <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z"/>
         </svg>
@@ -38,7 +38,7 @@ const navLinks = [
         ),
         badge: 4
     },
-    {   to: "/documents", label: "Documents", icon: (
+    {   to: "/tenant/document", label: "Documents", icon: (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" fill="currentColor">
             <path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM80 64h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16s7.2-16 16-16zm16 96H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm0 32v64H288V256H96zM240 416h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H240c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/>
         </svg>
