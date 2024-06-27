0b20ce5fccf50bb1f97d3e34b437d362887fd4a5
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-06-27 16:41:52 +0300

    refactor: Update MaintenanceRequestDetails layout for improved design consistency
    - Wrapped the entire maintenance request details section in a flex container with padding and margin adjustments.
    - Added background color, shadow, and rounded corners for better visual distinction.
    - Updated heading and section divider styling for improved readability.

--- Frontend/flash-rent/src/pages/tenant/partials/MaintenanceRequestDetails.jsx
+++ Frontend/flash-rent/src/pages/tenant/partials/MaintenanceRequestDetails.jsx
@@ -50,41 +50,42 @@ export default function MaintenanceRequestDetails() {
 
     return (
         <AuthenticatedLayout>
-            <div className="px-4 sm:px-0">
-                <h3 className="text-base font-semibold leading-7 text-gray-900 dark:text-white">{maintenanceRequest.title}</h3>
-            </div>
-            <div className="mt-6 border-t border-gray-100">
-                <div className="divide-y divide-gray-100">
-                    {renderField('Maintenance Issue', maintenanceRequest.title)}
-                    {renderField('Description', maintenanceRequest.description)}
-                    {renderField('Reported Date', maintenanceRequest.reportedDate)}
-                    {renderField('Status', maintenanceRequest.status)}
-                    {renderField('Maintenance scheduled date', maintenanceRequest.maintenanceScheduledDate)}
-                    {maintenanceRequest.completedDate && renderField('Completed date', maintenanceRequest.completedDate)}
-
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <div className="text-sm font-medium leading-6 text-gray-900 dark:text-white">
-                            Attachments
-                        </div>
-                        <div className="mt-2 text-sm text-gray-900 dark:text-white sm:col-span-2 sm:mt-0">
-                            <ul className="divide-y divide-gray-900 rounded-md border border-gray-900 dark:border-gray-100">
-                                {maintenanceRequest.attachments.map((attachment, index) => (
-                                    <li key={index} className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                                        <div className="flex w-0 flex-1 items-center">
-                                            <svg className="h-5 w-5 flex-shrink-0 dark:text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fillRule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clipRule="evenodd" />
-                                            </svg>
-                                            <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                                <span className="truncate font-medium text-gray-900 dark:text-white">{attachment.name}</span>
-                                                <span className="flex-shrink-0 dark:text-gray-400">{attachment.size}</span>
+            <div className="flex flex-col mt-6 px-4 py-4 md:col-span-2 md:row-span-2 bg-white dark:bg-gray-800 shadow-md rounded-lg">
+                <div className="px-4 sm:px-0">
+                    <h3 className="text-base font-semibold leading-7 text-gray-900 dark:text-white">{maintenanceRequest.title}</h3>
+                </div>
+                <div className="mt-6 border-t border-gray-100">
+                    <div className="divide-y divide-gray-100">
+                        {renderField('Maintenance Issue', maintenanceRequest.title)}
+                        {renderField('Description', maintenanceRequest.description)}
+                        {renderField('Reported Date', maintenanceRequest.reportedDate)}
+                        {renderField('Status', maintenanceRequest.status)}
+                        {renderField('Maintenance scheduled date', maintenanceRequest.maintenanceScheduledDate)}
+                        {maintenanceRequest.completedDate && renderField('Completed date', maintenanceRequest.completedDate)}
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <div className="text-sm font-medium leading-6 text-gray-900 dark:text-white">
+                                Attachments
+                            </div>
+                            <div className="mt-2 text-sm text-gray-900 dark:text-white sm:col-span-2 sm:mt-0">
+                                <ul className="divide-y divide-gray-900 rounded-md border border-gray-900 dark:border-gray-100">
+                                    {maintenanceRequest.attachments.map((attachment, index) => (
+                                        <li key={index} className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                                            <div className="flex w-0 flex-1 items-center">
+                                                <svg className="h-5 w-5 flex-shrink-0 dark:text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fillRule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clipRule="evenodd" />
+                                                </svg>
+                                                <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                                                    <span className="truncate font-medium text-gray-900 dark:text-white">{attachment.name}</span>
+                                                    <span className="flex-shrink-0 dark:text-gray-400">{attachment.size}</span>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="ml-4 flex-shrink-0">
-                                            <Link to="#" className="font-medium text-indigo-600 hover:text-indigo-500">Download</Link>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
+                                            <div className="ml-4 flex-shrink-0">
+                                                <Link to="#" className="font-medium text-indigo-600 hover:text-indigo-500">Download</Link>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
