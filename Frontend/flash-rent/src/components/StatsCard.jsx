07ad45a8ffef4edf83034c54af65419fa92521fb
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-06-27 16:46:06 +0300

    fix: Correct typo in className attribute for StatsCard component
    - Fixed a typo in the className attribute for the inline-flex div within the StatsCard component.

--- Frontend/flash-rent/src/components/StatsCard.jsx
+++ Frontend/flash-rent/src/components/StatsCard.jsx
@@ -12,15 +12,15 @@ export default function StatsCard ({icon, title, children, bgColor = "white"}) {
     }
     return (
         <div className={`p-8 h-30 md:h-64 flex items-center justify-center shadow-md rounded-lg ${bgColorClassName}`}>
-            <div ClassName="inline-flex flex-shrink-0 items-center justify-center rounded-lg mr-6">
+            <div className="inline-flex flex-shrink-0 items-center justify-center rounded-lg mr-6">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="h-10 w-10 text-gray-900" viewBox="0 0 576 512">
                     <path d={icon}/>
                 </svg>
             </div>
             <div className="ml-5">
-            <h2 className="block text-lg font-bold">{title}</h2>
-            {children}
-            </div>
+                <h2 className="block text-lg font-bold">{title}</h2>
+                {children}
+            </div>  
         </div>
     );
 };
