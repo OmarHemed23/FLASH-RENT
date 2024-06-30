9a425439618508954d745e8a8cc4571e5212833f
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-06-30 16:56:27 +0300

    feat: Register User, PropertyType, and Property models in the Django admin interface
    Registered the following models in the Django admin interface:
    - `User` model with `UserAdmin` customization for enhanced admin functionality.
    - `PropertyType` model to manage different types of properties.
    - `Property` model for managing properties including their details and status.

--- Backend/flashRent/base/admin.py
+++ Backend/flashRent/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, PropertyType, Property
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(PropertyType)
+admin.site.register(Property)
+
