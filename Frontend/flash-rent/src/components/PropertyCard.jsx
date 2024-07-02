80afb260b1e505b344f2e7d87114dcdf0f5b5aa6
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-07-02 14:28:07 +0300

    feat: Update PropertyCard component with new properties and styles
    - Changed the link path to dynamically use the property's name.
    - Updated the image source to use `property.image1` for better image handling.
    - Updated rent display to show "Monthly Rent: Ksh. {property.rent_amount}".
    - Added padding to the link for improved spacing.

--- Frontend/flash-rent/src/components/PropertyCard.jsx
+++ Frontend/flash-rent/src/components/PropertyCard.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 import PrimaryButton from './PrimaryButton';
 
 export default function PropertyCard ({property}) {
+  const link = `/property/${property.name}`
   return (
     <div className="border border-gray-900 dark:border-gray-100 rounded-lg overflow-hidden shadow-md">
-      <img src={property.image} alt={property.name} className="w-full h-48 object-cover" />
+      <img src={property.image1} alt={property.name} className="w-full h-48 object-cover" />
       <div className="p-4">
         <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-100">{property.name}</h3>
-        <p className="text-gray-900 dark:text-gray-100">{property.location}</p>
-        <p className="text-gray-900 dark:text-gray-100">{property.rent}</p>
-        <Link to={property.link}>
+        <p className="text-gray-900 dark:text-gray-100">Monthly Rent: Ksh. {property.rent_amount}</p>
+        <Link to={link} className="p-1.5">
           <PrimaryButton className="mt-2 block">View Details</PrimaryButton>
         </Link>
       </div>
